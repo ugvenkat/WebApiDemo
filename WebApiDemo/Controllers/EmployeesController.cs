f17d3f06eb5cd15ade97362ba4ff9e64bc76f2aa
Venkat Gandhi <ugvenkat@hotmail.com>
2022-11-26 11:35:42 -0600

    first commit

---
+++ WebApiDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.Configuration;
+//using System.Web.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public static IList<Employee> listEmp = new List<Employee>()
+        {
+        };
+
+        [AcceptVerbs("GET")]
+        public Employee RPCStyleMethodFetchFirstEmployees()
+        {
+            return listEmp.FirstOrDefault();
+        }
+
+
+        [HttpGet]
+        [ActionName("GetAllEmployees")]
+        public List<Employee> Get()
+        {
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+
+            myConnection.ConnectionString = ConfigurationManager.AppSettings["DefaultConnection"];
+
+            List<Employee> AllEmployee = new List<Employee>();
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * from tblEmployee";
+
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            while (reader.Read())
+            {
+                AllEmployee.Add(new Employee()
+                {
+                    EmployeeId = Convert.ToInt32(reader.GetValue(0)),
+                    Name = reader.GetValue(1).ToString(),
+                    ManagerId = Convert.ToInt32(reader.GetValue(2))
+                });
+            }
+            return AllEmployee;
+        }
+
+
+
+
+        [HttpGet]
+        [ActionName("GetEmployeeByID")]
+        public Employee Get(int id)
+        {
+            //return listEmp.First(e => e.ID == id);
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+
+            myConnection.ConnectionString = ConfigurationManager.AppSettings["DefaultConnection"];
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * from tblEmployee where EmployeeId=" + id + "";
+
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            Employee emp = null;
+            while (reader.Read())
+            {
+                emp = new Employee();
+                emp.EmployeeId = Convert.ToInt32(reader.GetValue(0));
+                emp.Name = reader.GetValue(1).ToString();
+                emp.ManagerId = Convert.ToInt32(reader.GetValue(2));
+            }
+            return emp;
+        }
+
+
+        [HttpPost]
+        public void AddEmployee(Employee employee)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = ConfigurationManager.AppSettings["DefaultConnection"];
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "INSERT INTO tblEmployee (EmployeeId,Name,ManagerId) Values (@EmployeeId,@Name,@ManagerId)";
+            sqlCmd.Connection = myConnection;
+            sqlCmd.Parameters.AddWithValue("@EmployeeId", employee.EmployeeId);
+            sqlCmd.Parameters.AddWithValue("@Name", employee.Name);
+            sqlCmd.Parameters.AddWithValue("@ManagerId", employee.ManagerId);
+            myConnection.Open();
+            int rowInserted = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+        }
+
+
+        [ActionName("DeleteEmployee")]
+        public void DeleteEmployeeByID(int id)
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = ConfigurationManager.AppSettings["DefaultConnection"];
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "delete from tblEmployee where EmployeeId=" + id + "";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            int rowDeleted = sqlCmd.ExecuteNonQuery();
+            myConnection.Close();
+        }
+    }
+}
